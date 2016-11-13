6ce90acdfd292751c7679a113198ba7d41640027
Miquel Martín <miqmago@gmail.com>
2016-11-13 18:08:17 +0100

    Initial commit

---
+++ src/angular-unsafe.js
@@ -0,0 +1,7 @@
+angular.module('angular.unsafe')
+
+    .filter('unsafe', ['$sce', function ($sce) {
+        return function (value, type) {
+            return $sce.trustAs(type || 'html', value);
+        }
+    }]);
